9a86cffcf994b3d2996051483d3a1757916709c7
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-01-28 16:50:20 -0700

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Bamazon-Node-MySQL
+
+## Overview 
+A Node.js & MySQL digital storefront. This is a command line Node app that mimics a beloved online retailer. 
--- bamazon.sql
+++ bamazon.sql
@@ -1,26 +1,31 @@
-DROP DATABASE IF EXISTS bamazon_db;
+-- Drop any preexisting "bamazon" Databases --
+DROP DATABASE IF EXISTS bamazon;
 
-CREATE DATABASE bamazon_db;
+-- Create a MySQL Database called "bamazon" --
+CREATE DATABASE bamazon;
+USE bamazon;
 
-USE bamazon_db;
-
+-- Create a table called "products" which will contain the store inventory --
 CREATE TABLE products (
-      item_id INT NOT NULL AUTO_INCREMENT,
-      product_name VARCHAR (50) NULL,
-      department_name VARCHAR (50) NULL,
-      price DECIMAL(10,2) NULL,
-      stock_quantity INT NULL,
+      item_id INT (11) AUTO_INCREMENT NOT NULL ,
+      product_name VARCHAR (50) NOT NULL,
+      department_name VARCHAR (50) NOT NULL,
+      price DECIMAL(10,2) NOT NULL,
+      stock_quantity INT (11) NOT NULL,
       PRIMARY KEY (id)
   );
 
+-- Insert data into the "products" table --
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("vanilla", 2.50, 100);
+VALUES ("Earth Rated Dog Waste Bags", "Pet Supplies", 11.99, 300),
+  ("Petmate Charcoal Replacement Water Filters", "Pet Supplies" 2.17, 120),
+  ("Hugger Mugger Tropic Yoga Mat", "Exercise & Fitness Equipment" 25.23, 200),
+  ("Hugger Mugger Batik Yoga Mat Bag", "Exercise & Fitness Equipment" 39.95, 90),
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("chocolate", 3.10, 120);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("strawberry", 3.25, 75);
+
+
+
 
 -- ### Alternative way to insert more than one row
 -- INSERT INTO products (flavor, price, quantity)
